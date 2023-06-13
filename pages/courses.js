1f8df3789340f9e22480a969c7a5efbb508d5699
OleanjiKingCode <holamilean56@gmail.com>
2023-06-13 20:45:52 +0100

    adding courses

--- pages/courses.js
+++ pages/courses.js
@@ -9,6 +9,7 @@ import {
   VStack,
   HStack,
   Heading,
+  Text,
 } from "@chakra-ui/react";
 import axios from "axios";
 
@@ -41,108 +42,108 @@ const Courses = () => {
   return (
     <SidebarWithHeader>
       <Flex>
-        {/* {deptData?.courses?.length === 0 ? (
+        {deptData?.courses?.length === 0 ? (
           userRole === "lecturer" ? (
-            <span>You can add courses as there are no courses available.</span>
+            <Box p={4} w="full" bg="white">
+              <Heading fontSize="lg" w="full" py="5" textAlign="center">
+                ADD COURSES
+              </Heading>
+              <Flex justifyContent="space-between" mb={4} gap={2} px="10%">
+                <Flex alignItems="center" justifyContent="center">
+                  <Text>{deptData?.name}</Text>
+                </Flex>
+                <Flex alignItems="center" justifyContent="center" gap={3}>
+                  <Text>Maximum Number Of Courses</Text>
+                  <Input placeholder="Number" w="20%" />
+                </Flex>
+
+                <Flex alignItems="center" justifyContent="center" gap={3}>
+                  <Text>Maximum Number Of Units</Text>
+                  <Input placeholder="Number" w="20%" />
+                </Flex>
+              </Flex>
+              <VStack mt={4}>
+                <HStack spacing={4}>
+                  {[...Array(4)].map((_, index) => (
+                    <VStack key={index} spacing={2} align="center">
+                      <Flex gap="1" alignItems="center" justifyContent="center">
+                        <span>{(index += 1)}</span>
+                        <Input placeholder="Course" flex="8" mr={2} />
+                        <Input placeholder="Number" flex="2" />
+                      </Flex>
+                    </VStack>
+                  ))}
+                </HStack>
+                <HStack spacing={4}>
+                  {[...Array(4)].map((_, index) => (
+                    <VStack key={index} spacing={2} align="center">
+                      <Flex gap="1" alignItems="center" justifyContent="center">
+                        <span>{(index += 5)}</span>
+                        <Input placeholder="Course" flex="8" mr={2} />
+                        <Input placeholder="Number" flex="2" />
+                      </Flex>
+                    </VStack>
+                  ))}
+                </HStack>
+                <HStack spacing={4}>
+                  {[...Array(4)].map((_, index) => (
+                    <VStack key={index} spacing={2} align="center">
+                      <Flex gap="1" alignItems="center" justifyContent="center">
+                        <span>{(index += 9)}</span>
+                        <Input placeholder="Course" flex="8" mr={2} />
+                        <Input placeholder="Number" flex="2" />
+                      </Flex>
+                    </VStack>
+                  ))}
+                </HStack>
+                <HStack spacing={4}>
+                  {[...Array(4)].map((_, index) => (
+                    <VStack key={index} spacing={2} align="center">
+                      <Flex gap="1" alignItems="center" justifyContent="center">
+                        <span>{(index += 13)}</span>
+                        <Input placeholder="Course" flex="8" mr={2} />
+                        <Input placeholder="Number" flex="2" />
+                      </Flex>
+                    </VStack>
+                  ))}
+                </HStack>
+                <HStack spacing={4}>
+                  {[...Array(4)].map((_, index) => (
+                    <VStack key={index} spacing={2} align="center">
+                      <Flex gap="1" alignItems="center" justifyContent="center">
+                        <span>{(index += 17)}</span>
+                        <Input placeholder="Course" flex="8" mr={2} />
+                        <Input placeholder="Number" flex="2" />
+                      </Flex>
+                    </VStack>
+                  ))}
+                </HStack>
+                <HStack spacing={4}>
+                  {[...Array(4)].map((_, index) => (
+                    <VStack key={index} spacing={2} align="center">
+                      <Flex gap="1" alignItems="center" justifyContent="center">
+                        <span>{(index += 21)}</span>
+                        <Input placeholder="Course" flex="8" mr={2} />
+                        <Input placeholder="Number" flex="2" />
+                      </Flex>
+                    </VStack>
+                  ))}
+                </HStack>
+              </VStack>
+              <Button my={4} colorScheme="green">
+                Save Changes
+              </Button>
+            </Box>
           ) : (
-            <span>
-              You cannot add courses as the lecturer hasn't added any yet.
-            </span>
+            <Box p={4} w="full" bg="white">
+              <Text>Contact your Lecturers to Update the courses</Text>
+            </Box>
           )
         ) : userRole === "lecturer" ? (
           <span>List of students that have added the courses.</span>
         ) : (
           <span>List of the courses you added.</span>
-        )} */}
-        <Box p={4} w="full" bg="white">
-          <Heading fontSize="lg" w="full" py="5" textAlign="center">
-            ADD COURSES
-          </Heading>
-          <Flex justify="space-between" mb={4} gap={2}>
-            <Flex>
-              <Input
-                placeholder="Department"
-                flex="8"
-                mr={2}
-                value={deptData?.name}
-              />
-            </Flex>
-            <Flex>
-              <Input placeholder="Text" flex="9" />
-              <Input placeholder="Number" flex="1" />
-            </Flex>
-
-            <Flex>
-              <Input placeholder="Text" flex="9" />
-              <Input placeholder="Number" flex="1" />
-            </Flex>
-          </Flex>
-          <VStack mt={4}>
-            <HStack spacing={4}>
-              {[...Array(4)].map((_, index) => (
-                <VStack key={index} spacing={2} align="center">
-                  <Flex>
-                    <Input placeholder="Text" flex="8" mr={2} />
-                    <Input placeholder="Number" flex="2" />
-                  </Flex>
-                </VStack>
-              ))}
-            </HStack>
-            <HStack spacing={4}>
-              {[...Array(4)].map((_, index) => (
-                <VStack key={index} spacing={2} align="center">
-                  <Flex>
-                    <Input placeholder="Text" flex="8" mr={2} />
-                    <Input placeholder="Number" flex="2" />
-                  </Flex>
-                </VStack>
-              ))}
-            </HStack>
-            <HStack spacing={4}>
-              {[...Array(4)].map((_, index) => (
-                <VStack key={index} spacing={2} align="center">
-                  <Flex>
-                    <Input placeholder="Text" flex="8" mr={2} />
-                    <Input placeholder="Number" flex="2" />
-                  </Flex>
-                </VStack>
-              ))}
-            </HStack>
-            <HStack spacing={4}>
-              {[...Array(4)].map((_, index) => (
-                <VStack key={index} spacing={2} align="center">
-                  <Flex>
-                    <Input placeholder="Text" flex="8" mr={2} />
-                    <Input placeholder="Number" flex="2" />
-                  </Flex>
-                </VStack>
-              ))}
-            </HStack>
-            <HStack spacing={4}>
-              {[...Array(4)].map((_, index) => (
-                <VStack key={index} spacing={2} align="center">
-                  <Flex>
-                    <Input placeholder="Text" flex="8" mr={2} />
-                    <Input placeholder="Number" flex="2" />
-                  </Flex>
-                </VStack>
-              ))}
-            </HStack>
-            <HStack spacing={4}>
-              {[...Array(4)].map((_, index) => (
-                <VStack key={index} spacing={2} align="center">
-                  <Flex>
-                    <Input placeholder="Text" flex="8" mr={2} />
-                    <Input placeholder="Number" flex="2" />
-                  </Flex>
-                </VStack>
-              ))}
-            </HStack>
-          </VStack>
-          <Button mt={4} colorScheme="green">
-            Save Changes
-          </Button>
-        </Box>
+        )}
       </Flex>
     </SidebarWithHeader>
   );

2153f4614088cf8549623939ba11562c2578d128
OleanjiKingCode <holamilean56@gmail.com>
2023-08-07 17:23:40 +0100

    update ability of lecturers

--- pages/courses.js
+++ pages/courses.js
@@ -1,10 +1,8 @@
 import SidebarWithHeader from "@/components/Sidebar";
 import React, { useEffect, useState } from "react";
-import { signIn, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import {
   Flex,
-  Box,
-  Input,
   Button,
   VStack,
   HStack,
@@ -32,9 +30,6 @@ const Courses = () => {
   const [userDept, setUserDept] = useState("");
   const toast = useToast();
 
-  const [maxCourseNo, setMaxCourseNo] = useState(0);
-
-  const [maxUnitsNo, setMaxUnitsNo] = useState(0);
   const [courseData, setCourseData] = useState([
     { name: "", units: "" },
     { name: "", units: "" },
@@ -54,18 +49,6 @@ const Courses = () => {
     { name: "", units: "" },
     { name: "", units: "" },
   ]);
-
-  const handleCourseChange = (index, value) => {
-    const updatedCourseData = [...courseData];
-    updatedCourseData[index].name = value;
-    setCourseData(updatedCourseData);
-  };
-
-  const handleUnitsChange = (index, value) => {
-    const updatedCourseData = [...courseData];
-    updatedCourseData[index].units = value;
-    setCourseData(updatedCourseData);
-  };
 
   const handleCheckboxChange = (index) => {
     const updatedCourseData = [...deptData.courses];
@@ -117,79 +100,6 @@ const Courses = () => {
     fetchData();
   }, [session, refetchData]);
 
-  const submitCourses = async () => {
-    if (maxCourseNo <= 8 || maxCourseNo > 16) {
-      toast({
-        title: "Maximum number of courses should be within 9-16",
-        description: "",
-        status: "warning",
-        duration: 4000,
-        isClosable: true,
-      });
-      return;
-    }
-    if (maxUnitsNo <= 10) {
-      toast({
-        title: "Maximum number of units to add is low",
-        description: "",
-        status: "warning",
-        duration: 4000,
-        isClosable: true,
-      });
-      return;
-    }
-
-    // Validate the input fields
-    let isValid = true;
-
-    const minRequiredItems = 9;
-    let validCourseData = [];
-
-    for (let i = 0; i < maxCourseNo; i++) {
-      const { name, units } = courseData[i];
-
-      if (name.trim() !== "" && units.trim() !== "") {
-        validCourseData.push(courseData[i]);
-      }
-    }
-
-    if (validCourseData.length < minRequiredItems) {
-      isValid = false;
-    }
-
-    if (isValid) {
-      try {
-        const result = await axios.put(`/api/Dept/${deptData?.name}`, {
-          courses: validCourseData,
-          maxCourses: maxCourseNo,
-          maxUnits: maxUnitsNo,
-        });
-        if (result) {
-          setRefetchData((prevValue) => !prevValue);
-          setRefetchData(true);
-          toast({
-            title: "Successfully added Courses",
-            description: "",
-            status: "success",
-            duration: 4000,
-            isClosable: true,
-          });
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    } else {
-      toast({
-        title: "Input at least 9 courses ",
-        description: "",
-        status: "warning",
-        duration: 4000,
-        isClosable: true,
-      });
-      return;
-    }
-  };
-
   const chosenCourses = async () => {
     let coursesWithCheckboxTrue = [];
     let amountOfUnits = 0;
@@ -236,150 +146,18 @@ const Courses = () => {
       <Flex>
         {deptData?.courses?.length === 0 ? (
           userRole === "Lecturer" ? (
-            <Box p={4} w="full" bg="white">
-              <Heading fontSize="lg" w="full" py="5" textAlign="center">
-                ADD COURSES
-              </Heading>
-              <Flex justifyContent="space-between" mb={4} gap={2} px="10%">
-                <Flex alignItems="center" justifyContent="center">
-                  <Text>{deptData?.name}</Text>
-                </Flex>
-                <Flex alignItems="center" justifyContent="center" gap={3}>
-                  <Text>Maximum Number Of Courses</Text>
-                  <Input
-                    placeholder="Number"
-                    onChange={(e) => setMaxCourseNo(e.target.value)}
-                    value={maxCourseNo}
-                    w="20%"
-                  />
-                </Flex>
-
-                <Flex alignItems="center" justifyContent="center" gap={3}>
-                  <Text>Maximum Number Of Units</Text>
-                  <Input
-                    placeholder="Number"
-                    onChange={(e) => setMaxUnitsNo(e.target.value)}
-                    value={maxUnitsNo}
-                    w="20%"
-                  />
-                </Flex>
-              </Flex>
-              <VStack mt={4}>
-                <HStack spacing={4}>
-                  {[...Array(4)].map((_, index) => (
-                    <VStack key={index} spacing={2} align="center">
-                      <Flex gap="1" alignItems="center" justifyContent="center">
-                        <span>{(index += 1)}</span>
-                        <Input
-                          id={`course-${index}`}
-                          placeholder="Course"
-                          flex="8"
-                          mr={2}
-                          value={courseData[index].name}
-                          onChange={(e) =>
-                            handleCourseChange(index, e.target.value)
-                          }
-                        />
-                        <Input
-                          id={`course-${index}`}
-                          placeholder="Number"
-                          flex="2"
-                          value={courseData[index].units}
-                          onChange={(e) =>
-                            handleUnitsChange(index, e.target.value)
-                          }
-                        />
-                      </Flex>
-                    </VStack>
-                  ))}
-                </HStack>
-                <HStack spacing={4}>
-                  {[...Array(4)].map((_, ind) => (
-                    <VStack key={ind} spacing={2} align="center">
-                      <Flex gap="1" alignItems="center" justifyContent="center">
-                        <span>{(ind += 5)}</span>
-                        <Input
-                          id={`course-${ind + 5}`}
-                          placeholder="Course"
-                          flex="8"
-                          mr={2}
-                          value={courseData[ind].name}
-                          onChange={(e) =>
-                            handleCourseChange(ind, e.target.value)
-                          }
-                        />
-                        <Input
-                          id={`course-${ind + 5}`}
-                          placeholder="Number"
-                          flex="2"
-                          value={courseData[ind].units}
-                          onChange={(e) =>
-                            handleUnitsChange(ind, e.target.value)
-                          }
-                        />
-                      </Flex>
-                    </VStack>
-                  ))}
-                </HStack>
-                <HStack spacing={4}>
-                  {[...Array(4)].map((_, i) => (
-                    <VStack key={i} spacing={2} align="center">
-                      <Flex gap="1" alignItems="center" justifyContent="center">
-                        <span>{(i += 9)}</span>
-                        <Input
-                          id={`course-${i + 9}`}
-                          placeholder="Course"
-                          flex="8"
-                          mr={2}
-                          value={courseData[i].name}
-                          onChange={(e) =>
-                            handleCourseChange(i, e.target.value)
-                          }
-                        />
-                        <Input
-                          id={`course-${i + 9}`}
-                          placeholder="Number"
-                          flex="2"
-                          value={courseData[i].units}
-                          onChange={(e) => handleUnitsChange(i, e.target.value)}
-                        />
-                      </Flex>
-                    </VStack>
-                  ))}
-                </HStack>
-                <HStack spacing={4}>
-                  {[...Array(4)].map((_, inde) => (
-                    <VStack key={inde} spacing={2} align="center">
-                      <Flex gap="1" alignItems="center" justifyContent="center">
-                        <span>{(inde += 13)}</span>
-                        <Input
-                          id={`course-${inde + 13}`}
-                          placeholder="Course"
-                          flex="8"
-                          mr={2}
-                          value={courseData[inde].name}
-                          onChange={(e) =>
-                            handleCourseChange(inde, e.target.value)
-                          }
-                        />
-                        <Input
-                          id={`course-${inde + 13}`}
-                          placeholder="Number"
-                          flex="2"
-                          value={courseData[inde].units}
-                          onChange={(e) =>
-                            handleUnitsChange(inde, e.target.value)
-                          }
-                        />
-                      </Flex>
-                    </VStack>
-                  ))}
-                </HStack>
-              </VStack>
-              <Button my={4} colorScheme="green" onClick={submitCourses}>
-                Save Changes
-              </Button>
-            </Box>
+            <Flex
+              p={4}
+              w="full"
+              minH="80vh"
+              bg="white"
+              fontWeight="semibold"
+              fontSize="lg"
+              justifyContent="center"
+              alignItems="center"
+            >
+              Contact Admin to update the courses
+            </Flex>
           ) : (
             <Flex
               p={4}
